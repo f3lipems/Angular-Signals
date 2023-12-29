e456e0cc8d04896037c5b4312089a15b0d820929
Felipe <felipe@visionar.io>
2023-12-29 14:33:59 -0300

    Set and Update Signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule, JsonPipe } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -11,4 +11,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'signals';
+
+  protected counterSignal = signal(1);
+  protected objectSignal = signal({action: 'none', value: 1});
+
+  // Set: Add new value to Signal
+  signalSet() {
+    this.counterSignal.set(0);
+  }
+
+  // Update: Allow you to use the current value;
+  signalUpdate() {
+    this.counterSignal.update(current => current + 1)
+  }
 }
